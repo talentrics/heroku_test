a23ecb549bf02ba7df41ed8ae0ecafd8cfe14b98
talentrics <daniel@talentrics.com>
2020-09-18 21:01:44 -0400

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,3 @@ from flask import url_for
 app = Flask(__name__)
 
 from app import views
-from app import admin_views
--- app/views.py
+++ app/views.py
@@ -6,72 +6,11 @@ from flask import render_template, url_for, request, redirect
 def index():
     return render_template("/public/index.html")
 
-@app.route("/jinja")
-def jinja():
-	    # Strings
-	my_name = "Julian"
 
-	# Integers
-	my_age = 30
+@app.route("/about")
+def index():
+    return render_template("/public/about.html")
 
-	# Lists
-	langs = ["Python", "JavaScript", "Bash", "Ruby", "C", "Rust"]
-
-	# Dictionaries
-	friends = {
-	    "Tony": 43,
-	    "Cody": 28,
-	    "Amy": 26,
-	    "Clarissa": 23,
-	    "Wendell": 39
-	}
-
-	# Tuples
-	colors = ("Red", "Blue")
-
-	# Booleans
-	cool = True
-
-	# Classes
-	class GitRemote:
-	    def __init__(self, name, description, domain):
-	        self.name = name
-	        self.description = description 
-	        self.domain = domain
-
-	    def clone(self, repo):
-	        return f"Cloning into {repo}"
-
-	my_remote = GitRemote(
-	    name="Learning Flask",
-	    description="Learn the Flask web framework for Python",
-	    domain="https://github.com/Julian-Nash/learning-flask.git"
-	)
-
-	# Functions
-	def repeat(x, qty=1):
-	    return x * qty
-	    
-	return render_template(
-	    "public/jinja.html", my_name=my_name, my_age=my_age, langs=langs,
-	    friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
-	    my_remote=my_remote, repeat=repeat
-	)
-
-@app.route("/sign_up", methods=["GET", "POST"])
-def sign_up():
-
-    if request.method == "POST":
-
-        req = request.form
-
-        username = req.get("username")
-        email = req["email"]
-        password = request.form["password"]
-
-        # You could also use 
-        password = request.form.get("password")
-
-        return redirect(request.url)
-
-    return render_template("public/sign_up.html")
+    @app.route("/page3")
+def index():
+    return render_template("/public/page3.html")
