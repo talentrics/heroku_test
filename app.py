219eaf4a08693a018767670616bcd650fc681644
talentrics <daniel@talentrics.com>
2020-09-20 08:37:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect
 
 import pandas as pd
 from threading import Thread
-from simplejson import json
+import simplejson as json
 import requests
 import datetime
 import jinja2
